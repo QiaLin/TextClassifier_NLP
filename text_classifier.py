84e51d82275f5aebcc47d298826095fb61971af0
QiaLin <lin.qia1@northeastern.edu>
2022-08-06 01:21:10 -0400

    Update text_classifier.py

--- text_classifier.py
+++ text_classifier.py
@@ -11,7 +11,11 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
+
+##LSTM
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 
 
