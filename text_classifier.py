18a46660ae71f93ed59bdfb14386e3ecc042c622
QiaLin <lin.qia1@northeastern.edu>
2022-08-05 23:27:24 -0400

    Update text_classifier.py

--- text_classifier.py
+++ text_classifier.py
@@ -12,7 +12,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-nltk.download('stopwords')
+nltk.download('all')
 
 wn = nltk.WordNetLemmatizer()
 ps = nltk.PorterStemmer()
